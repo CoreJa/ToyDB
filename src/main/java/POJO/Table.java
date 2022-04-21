a6e3b063d9097ed24b021d36389f598ba1a88f5c
CoreJa <i@coreja.com>
2022-04-21 15:31:31 -0400

    update by Fog1ess
    Signed-off-by: CoreJa <i@coreja.com>

--- src/main/java/POJO/Table.java
+++ src/main/java/POJO/Table.java
@@ -2,15 +2,19 @@ package POJO;
 
 import adaptors.StatementVisitorAdaptor;
 import net.sf.jsqlparser.statement.StatementVisitor;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class Table implements Serializable {
+    private String tableName;
     private List<String> columnNames;
-    private List<String> types;
+    private List<DataRow.Types> types;
     private Map<String, DataRow> data;//key: primary key; value: data record
 
     public Table(List<String> columnNames, Map<String, DataRow> data) {
@@ -19,6 +23,17 @@ public class Table implements Serializable {
     }
 
     public Table(CreateTable createTableStatement) {
-        StatementVisitor statementVisitor = new StatementVisitorAdaptor();
+        // define the name and dataType of each column
+        this.tableName = createTableStatement.getTable().getName();
+        List<ColumnDefinition> columnDefinitionList = createTableStatement.getColumnDefinitions();
+        columnNames = new ArrayList<>();
+        types = new ArrayList<>();
+        for(Iterator<ColumnDefinition> iter = columnDefinitionList.iterator(); iter.hasNext();) {
+            ColumnDefinition def = iter.next();
+            columnNames.add(def.getColumnName());
+            if(def.getColDataType().getDataType().toLowerCase().compareTo("string") == 0) {
+                types.add(DataRow.Types.STRING);
+            }
+        }
     }
 }
