c9c98f89ff1057f582d2633e42cbc8a01582bcb2
zhy3213 <haoyang_zhang@hotmail.com>
2022-04-23 14:28:28 -0400

    Changes by core

---
+++ src/main/java/utils/NotImplementedException.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class NotImplementedException extends RuntimeException {
+    public NotImplementedException() {
+        super("SQL NotImplemented Exception: Some Executions have not been implemented yet.");
+    }
+
+    public NotImplementedException(String text) {
+        super("SQL NotImplemented Exception: " + text + "has not been implemented yet.");
+    }
+}
