51468741832602f2a16283f12a892a753b18f19f
CoreJa <i@coreja.com>
2022-04-26 18:23:32 -0400

    adding +-*/ operation to DataGrid.java
    Signed-off-by: CoreJa <i@coreja.com>

--- src/main/java/POJO/DataGrid.java
+++ src/main/java/POJO/DataGrid.java
@@ -32,16 +32,12 @@ public class DataGrid implements Serializable {
     }
 
     public boolean compareTo(DataGrid dataGrid) {
-        if (this.type != dataGrid.type) {
-            throw new ExecutionException("Can't compare type: " + this.type + " with type " + dataGrid.type);
-        }
+        this.checkType(dataGrid, "compare");
         return this.data.toString().compareTo(dataGrid.toString()) == 0;
     }
 
     public boolean greatThan(DataGrid dataGrid) {
-        if (this.type != dataGrid.type) {
-            throw new ExecutionException("Can't compare type: " + this.type + " with type " + dataGrid.type);
-        }
+        this.checkType(dataGrid, "compare");
         if (this.type == Type.STRING) {
             return this.data.toString().compareTo(dataGrid.toString()) > 0;
         } else {
@@ -50,13 +46,49 @@ public class DataGrid implements Serializable {
     }
 
     public boolean minorThan(DataGrid dataGrid) {
-        if (this.type != dataGrid.type) {
-            throw new ExecutionException("Can't compare type: " + this.type + " with type " + dataGrid.type);
-        }
+        this.checkType(dataGrid, "compare");
         if (this.type == Type.STRING) {
             return this.data.toString().compareTo(dataGrid.toString()) < 0;
         } else {
             return (int) this.data < (int) dataGrid.data;
         }
+    }
+
+    private void checkType(DataGrid dataGrid, String operation) {
+        if (this.type != dataGrid.type) {
+            throw new ExecutionException("Can't " + operation + " type: " + this.type + " with type " + dataGrid.type);
+        }
+    }
+
+    public void add(DataGrid dataGrid) {
+        checkType(dataGrid, "add");
+        if (this.type != Type.INT || dataGrid.type != Type.INT) {
+            throw new ExecutionException("Type " + this.type + "doesn't support Addition operation.");
+        }
+        this.data = (int) this.data + (int) dataGrid.data;
+    }
+
+    public void sub(DataGrid dataGrid) {
+        checkType(dataGrid, "subtract");
+        if (this.type != Type.INT || dataGrid.type != Type.INT) {
+            throw new ExecutionException("Type " + this.type + "doesn't support Subtraction operation.");
+        }
+        this.data = (int) this.data - (int) dataGrid.data;
+    }
+
+    public void mul(DataGrid dataGrid) {
+        checkType(dataGrid, "multiply");
+        if (this.type != Type.INT || dataGrid.type != Type.INT) {
+            throw new ExecutionException("Type " + this.type + "doesn't support Multiplication operation.");
+        }
+        this.data = (int) this.data * (int) dataGrid.data;
+    }
+
+    public void div(DataGrid dataGrid) {
+        checkType(dataGrid, "divide");
+        if (this.type != Type.INT || dataGrid.type != Type.INT) {
+            throw new ExecutionException("Type " + this.type + "doesn't support Division operation.");
+        }
+        this.data = (int) this.data / (int) dataGrid.data;
     }
 }
